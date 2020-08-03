3fd288af5633aa10cecc9ba305bb0d4c5941f93a
Timo Sugliani <timo.sugliani@gmail.com>
2020-08-03 20:38:48 +0200

    Bump to Debian 10.5, HashiCorp apt repo, packer fixes
    Signed-off-by: Timo Sugliani <timo.sugliani@gmail.com>

--- build-minimal.sh
+++ build-minimal.sh
@@ -2,5 +2,5 @@
 
 packer build \
     --var-file="debian-builder.json" \
-    --var-file="debian-minimal-version-10.4.0.json" \
+    --var-file="debian-minimal-version-10.5.0.json" \
     debian-minimal.json
