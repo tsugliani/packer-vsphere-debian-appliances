7233a0106c110738bcd81a58a1c9f8833dff03bb
Timo Sugliani <timo.sugliani@gmail.com>
2021-05-06 12:49:32 +0200

    Adding Debian 10.9 version & fixes for behavior change in latest packer version of output directory
    Signed-off-by: Timo Sugliani <timo.sugliani@gmail.com>

--- build-toolbox.sh
+++ build-toolbox.sh
@@ -2,5 +2,5 @@
 
 packer build \
     --var-file="debian-builder.json" \
-    --var-file="debian-toolbox-version-10.8.0.json" \
+    --var-file="debian-toolbox-version-10.9.0.json" \
     debian-toolbox.json
--- postprocess-ova-properties/add_ovf_properties.sh
+++ postprocess-ova-properties/add_ovf_properties.sh
@@ -4,22 +4,21 @@
 
 OUTPUT_PATH="../output-${APPLIANCE_NAME}"
 
-rm -f ${OUTPUT_PATH}/${APPLIANCE_NAME}/${APPLIANCE_NAME}.mf
+rm -f ${OUTPUT_PATH}/${APPLIANCE_NAME}.mf
 
 sed "s/{{APPLIANCE_VERSION}}/${APPLIANCE_VERSION}/g" appliance.xml.template > appliance.xml
 
 if [ "$(uname)" == "Darwin" ]; then
-    sed -i .bak1 's/<VirtualHardwareSection>/<VirtualHardwareSection ovf:transport="com.vmware.guestInfo">/g' ${OUTPUT_PATH}/${APPLIANCE_NAME}/${APPLIANCE_NAME}.ovf
-    sed -i .bak2 "/    <\/vmw:BootOrderSection>/ r appliance.xml" ${OUTPUT_PATH}/${APPLIANCE_NAME}/${APPLIANCE_NAME}.ovf
-    sed -i .bak3 '/^      <vmw:ExtraConfig ovf:required="false" vmw:key="nvram".*$/d' ${OUTPUT_PATH}/${APPLIANCE_NAME}/${APPLIANCE_NAME}.ovf
-    sed -i .bak4 "/^    <File ovf:href=\"${APPLIANCE_NAME}-file1.nvram\".*$/d" ${OUTPUT_PATH}/${APPLIANCE_NAME}/${APPLIANCE_NAME}.ovf
+    sed -i .bak1 's/<VirtualHardwareSection>/<VirtualHardwareSection ovf:transport="com.vmware.guestInfo">/g' ${OUTPUT_PATH}/${APPLIANCE_NAME}.ovf
+    sed -i .bak2 "/    <\/vmw:BootOrderSection>/ r appliance.xml" ${OUTPUT_PATH}/${APPLIANCE_NAME}.ovf
+    sed -i .bak3 '/^      <vmw:ExtraConfig ovf:required="false" vmw:key="nvram".*$/d' ${OUTPUT_PATH}/${APPLIANCE_NAME}.ovf
+    sed -i .bak4 "/^    <File ovf:href=\"${APPLIANCE_NAME}-file1.nvram\".*$/d" ${OUTPUT_PATH}/${APPLIANCE_NAME}.ovf
 else
-    sed -i 's/<VirtualHardwareSection>/<VirtualHardwareSection ovf:transport="com.vmware.guestInfo">/g' ${OUTPUT_PATH}/${APPLIANCE_NAME}/${APPLIANCE_NAME}.ovf
-    sed -i "/    <\/vmw:BootOrderSection>/ r appliance.xml" ${OUTPUT_PATH}/${APPLIANCE_NAME}/${APPLIANCE_NAME}.ovf
-    sed -i '/^      <vmw:ExtraConfig ovf:required="false" vmw:key="nvram".*$/d' ${OUTPUT_PATH}/${APPLIANCE_NAME}/${APPLIANCE_NAME}.ovf
-    sed -i "/^    <File ovf:href=\"${APPLIANCE_NAME}-file1.nvram\".*$/d" ${OUTPUT_PATH}/${APPLIANCE_NAME}/${APPLIANCE_NAME}.ovf
+    sed -i 's/<VirtualHardwareSection>/<VirtualHardwareSection ovf:transport="com.vmware.guestInfo">/g' ${OUTPUT_PATH}/${APPLIANCE_NAME}.ovf
+    sed -i "/    <\/vmw:BootOrderSection>/ r appliance.xml" ${OUTPUT_PATH}/${APPLIANCE_NAME}.ovf
+    sed -i '/^      <vmw:ExtraConfig ovf:required="false" vmw:key="nvram".*$/d' ${OUTPUT_PATH}/${APPLIANCE_NAME}.ovf
+    sed -i "/^    <File ovf:href=\"${APPLIANCE_NAME}-file1.nvram\".*$/d" ${OUTPUT_PATH}/${APPLIANCE_NAME}.ovf
 fi
 
-ovftool ${OUTPUT_PATH}/${APPLIANCE_NAME}/${APPLIANCE_NAME}.ovf ${OUTPUT_PATH}/${APPLIANCE_NAME}.ova
-rm -rf ${OUTPUT_PATH}/${APPLIANCE_NAME}
+ovftool ${OUTPUT_PATH}/${APPLIANCE_NAME}.ovf ${OUTPUT_PATH}/${APPLIANCE_NAME}.ova
 rm -f appliance.xml
